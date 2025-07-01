59d87468fd7a4710ccb426a33ea1393ed48b0064
Luke Kim <luke.kimyh@gmail.com>
2025-06-30 19:37:02 -0500

    changing direction of website to Pokemon search API, renamed home.html to index.html

--- node practice/index.js
+++ node practice/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
-const {readFile} = require('fs').promises;
+const axios= require('axios');
+const path = require('path');
+const readFile = require('fs').promises;
+
 const app = express();
 
-app.get('/', async (request, response) => {
-    response.send(await readFile('./home.html', 'utf8'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/pokemon/:name', async (req, res) => {
+    const { name } = req.params;
+
+    try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
+        res.json(response.data);
+    } catch (error) {
+        res.status(404).json({ error: 'Pokémon not found' });
+    }
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
+app.listen(process.env.PORT || 3000, () => {
+    console.log('App available on http://localhost:3000');
+});
