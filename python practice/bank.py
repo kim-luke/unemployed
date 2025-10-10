f28a543848c73565d0855309e8bc99a93f278fc8
kim.luke <luke.kimyh@gmail.com>
2025-10-09 22:00:24 -0500

    Adding levels 1 and 2 of banking system practice

--- python practice/bank.py
+++ python practice/bank.py
@@ -1,1 +1,31 @@
+class Bank:
 
+    def __init__(self):
+        self.accounts = {}
+    
+    def create(self, account_id: int, initial_balance: int) -> bool:
+
+        if account_id in self.accounts:
+            return False
+
+        self.accounts[account_id] = initial_balance
+        return True
+    
+    def deposit(self, account_id: int, amount: int) -> int:
+        if account_id not in self.accounts:
+            return -1
+
+        self.accounts[account_id] += amount
+        return self.accounts[account_id]
+    
+    def transfer(self, from_account: int, to_account: int, amount: int) -> bool:
+
+        if from_account not in self.accounts or to_account not in self.accounts:
+            return False
+
+        if self.accounts[from_account] < amount:
+            return False
+
+        self.accounts[from_account] -= amount
+        self.accounts[to_account] += amount
+        return True
